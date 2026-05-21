2084a0da7f67a34c9b5636f77d04be648b205966
Apurv Singh <apurvsingh934@gmail.com>
2026-05-21 12:35:00 +0530

    server created

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected");
+    });
+    await mongoose.connect(`${process.env.MONGODB_URL}/pingup`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
---
+++ server/inngest/index.js
@@ -0,0 +1,63 @@
+import { Inngest } from "inngest";
+import User from "../models/User.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "pingup-app" });
+
+// Inngest function to save user data into databse
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk", triggers: { event: "clerk/user.created" } },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    let username = email_addresses[0].email_address.split("@")[0];
+
+    // check availability of username
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      username = username + Math.floor(Math.random() * 10000);
+    }
+
+    const userdata = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      full_name: first_name + " " + last_name,
+      profile_picture: image_url,
+      username,
+    };
+
+    await User.create(userdata);
+  },
+);
+
+// Inngest function to update user data into databse
+const syncUserUpdate = inngest.createFunction(
+  { id: "update-user-from-clerk", triggers: { event: "clerk/user.updated" } },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const updatedUserData = {
+      email: email_addresses[0].email_address,
+      full_name: first_name + " " + last_name,
+      profile_picture: image_url,
+    };
+
+    await User.findByIdAndUpdate(id, updatedUserData);
+  },
+);
+
+// Inngest function to delete user data into databse
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk", triggers: { event: "clerk/user.deleted" } },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await User.findByIdAndDelete(id);
+  },
+);
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUserCreation, syncUserUpdate, syncUserDeletion];
